870c45d140e1f7bfb66c16f8d86c1d8fd1cc704b
JohanChane <johanchane@qq.com>
2023-07-01 21:20:25 +0800

    Use password

--- README_CN.md
+++ README_CN.md
@@ -1,6 +1,6 @@
 # Intuitionistic backup
 
-Language: [English](./README.md) | [中文](./README_CN.md)
+[English](./README.md) | [中文](./README_CN.md)
 
 ## 平台
 
@@ -11,24 +11,55 @@ Language: [English](./README.md) | [中文](./README_CN.md)
 安装 & 运行:
 
 ```sh
-git clone https://github.com/JohanChane/intuit-backup.git
+git clone https://github.com/JohanChane/intuit-backup.git --depth 1
+# 配置 ~/.cofnig/intuitbackup/config.toml
 cd intuit-backup
 ./intuitbackup
 ```
 
 intuit-backup 的相关文件:
 
--   config: ~/.config/intuitbackup/intuitbackup.conf
+-   config: ~/.config/intuitbackup/config.toml
     -   dest_dir: 存放备份的文件的压缩包。
     -   record_dir: 存放需要备份的文件的记录文件。
     -   info_dir: 存放需要备份的系统信息。还有其下的 info_cmd 文件用于生成需要备份的系统信息。
+    -   password: 压缩包的解压密码。注意保管。
 
 选项:
 
 -   backup: 备份一个 record 记录的文件。
 -   extract: 解压一个备份压缩包。
--   restore: 从一个备份压缩包中恢复文件。
+-   restore: 从一个备份压缩包中恢复文件。注意: 会覆盖已有的文件。
 -   list_files: 列出备份压缩包的文件。
 -   check_record: 检查 record 记录的文件。比如: 文件是否存在。
 -   cat_record: 查看 record 文件的内容
--   backup_info: 备份系统信息。
+-   gen_info: 生成系统信息。
+
+For example: `~/IntuitBackup/Records/confs`
+
+`#` 开头是注释。
+
+```
+# ## IntuitBackup
+# 备份 gen_info 生成的系统信息
+~/IntuitBackup/Info
+~/.config/intuitbackup
+~/IntuitBackup/Records
+
+# ## system config files
+/etc/fstab
+/boot/grub/grub.cfg
+/etc/default/grub
+/etc/sudoers
+/etc/environment
+~/.xinitrc
+~/.xprofile
+~/.profile
+
+# ## zsh
+~/.zshrc
+```
+
+## Doc
+
+[Doc](./Doc)
--- intuitbackuputil.py
+++ intuitbackuputil.py
@@ -1,90 +1,110 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import os, time, configparser
+import os, sys, time
+import toml
+from dataclasses import dataclass
+
+@dataclass
+class Config:
+    dest_dir: str
+    record_dir: str
+    info_dir: str
+    password: str
 
 class IntuitBackup():
     def __init__(self):
         self.__username = os.getenv('SUDO_USER') or os.getenv('USER')
-        if self.__username == "root":
+        if not self.__username:
+            sys.stderr.write("Failed to get username\n")
+            sys.exit(1)
+        elif self.__username == "root":
             self.__home_dir = "/root"
         else:
             self.__home_dir = os.path.join("/home", self.__username)
 
-        config_path = os.path.join(self.__home_dir, ".config", "intuitbackup", "intuitbackup.conf")
-        config = configparser.ConfigParser()
-        config.read(config_path)
-        self.__dest_dir = config.get("base", "dest_dir").strip()
-        if self.__dest_dir.startswith("~"):
-            self.__dest_dir = self.__home_dir + self.__dest_dir[1:]
-        self.__record_dir = config.get("base", "record_dir").strip()
-        if self.__record_dir.startswith("~"):
-            self.__record_dir = self.__home_dir + self.__record_dir[1:]
-        self.__info_dir = config.get("base", "info_dir").strip()
-        if self.__info_dir.startswith("~"):
-            self.__info_dir = self.__home_dir + self.__info_dir[1:]
+        config_path = os.path.join(self.__home_dir, ".config", "intuitbackup", "config.toml")
+        
+        with open(config_path, 'r') as file:
+            config_dict = toml.load(file)
+        self.__config = Config(**config_dict)
+        
+        self.__config.dest_dir = self.__expanduser(self.__config.dest_dir)
+        self.__config.record_dir = self.__expanduser(self.__config.record_dir)
+        self.__config.info_dir = self.__expanduser(self.__config.info_dir)
+
+        if os.geteuid() != 0:
+            os.makedirs(self.__config.dest_dir, exist_ok=True)
+            os.makedirs(self.__config.record_dir, exist_ok=True)
+            os.makedirs(self.__config.info_dir, exist_ok=True)
+
+        return
 
     def backup(self, record_name):
-        archive_name = record_name + time.strftime('%Y-%m-%d_%H-%M-%S', time.localtime()) + ".tar.gz"
-        archive_path = os.path.join(self.__dest_dir, archive_name)
+        archive_name = record_name + '-' + time.strftime('%Y%m%d_%H%M%S', time.localtime()) + ".tar.gz"
+        archive_path = os.path.join(self.__config.dest_dir, archive_name)
 
-        record_path = os.path.join(self.__record_dir, record_name)
+        record_path = os.path.join(self.__config.record_dir, record_name)
         files = self.__get_files(record_path)
         files_with_quote = []
         for f in files:
             files_with_quote.append(f'"{f}"')
-        cmd = f'tar -cvP -f "{archive_path}" {" ".join(files_with_quote)}'
+        #cmd = f'tar -acvP -f "{archive_path}" {" ".join(files_with_quote)}'
+        cmd = f"tar -acvP -f - {' '.join(files_with_quote)} | gpg --batch --yes --symmetric --cipher-algo AES256 --passphrase '{self.__config.password}' --output '{archive_path}.gpg'"
         os.system(cmd)
         print(f'Backup successfully: "{archive_path}"')
 
     def check_record(self, record_name):
-        record_path = os.path.join(self.__record_dir, record_name)
+        record_path = os.path.join(self.__config.record_dir, record_name)
         files = self.__get_files(record_path)
         for f in files:
             if not os.path.exists(f):
-                print(f'"f" is not exist.')
+                print(f'"{f}" is not exist.')
 
     def cat_record(self, record_name):
-        record_path = os.path.join(self.__record_dir, record_name)
+        record_path = os.path.join(self.__config.record_dir, record_name)
         files = self.__get_files(record_path)
         print("\n".join(files))
 
     def extract(self, archive_name):
         oldwd = os.getcwd()
-        os.chdir(self.__dest_dir)
+        os.chdir(self.__config.dest_dir)
 
         file_name_no_suffix = archive_name.split(".")[0]
         os.mkdir(file_name_no_suffix)
-        archive_path = os.path.join(self.__dest_dir, archive_name)
-        cmd = f'tar -xv -f "{archive_path}" -C "{file_name_no_suffix}"'
+        archive_path = os.path.join(self.__config.dest_dir, archive_name)
+        #cmd = f'tar -xv -p --same-owner -f "{archive_path}" -C "{file_name_no_suffix}"'
+        cmd = f"gpg --batch --yes --passphrase '{self.__config.password}' -d {archive_path} | tar -xv -p --same-owner -f - -C '{file_name_no_suffix}'"
         os.system(cmd)
 
         os.chdir(oldwd)
 
     def restore(self, archive_name):
-        archive_path = os.path.join(self.__dest_dir, archive_name)
-        cmd = f'tar -xvP -f {archive_path}'
+        archive_path = os.path.join(self.__config.dest_dir, archive_name)
+        #cmd = f'tar -xvP -p --same-owner -f {archive_path}'
+        cmd = f"gpg --batch --yes --passphrase '{self.__config.password}' -d {archive_path} | tar -xvP -p --same-owner -f -"
         os.system(cmd)
 
     def list_files(self, archive_name):
-        archive_path = os.path.join(self.__dest_dir, archive_name)
-        cmd = f'tar -tv -f {archive_path}'
+        archive_path = os.path.join(self.__config.dest_dir, archive_name)
+        #cmd = f'tar -tv -f {archive_path}'
+        cmd = f"gpg --batch --yes --passphrase '{self.__config.password}' -d {archive_path} | tar -tv -f -"
         os.system(cmd)
     
-    def backup_info(self):
+    def gen_info(self):
         oldwd = os.getcwd()
-        os.chdir(self.__info_dir)
+        os.chdir(self.__config.info_dir)
 
-        cmd = f'cd "{self.__info_dir}"; ./info_cmd'
+        cmd = f'cd "{self.__config.info_dir}"; ./info_cmd'
         print(cmd)
         os.system(cmd)
 
         os.chdir(oldwd)
 
     def get_dest_dir(self):
-        return self.__dest_dir
+        return self.__config.dest_dir
     def get_record_dir(self):
-        return self.__record_dir
+        return self.__config.record_dir
     def get_username(self):
         return self.__username
     def get_home_dir(self):
@@ -98,8 +118,10 @@ class IntuitBackup():
                 line = line.strip()
                 if not line or line.startswith("#"):
                     continue
-                elif line.startswith("~"):
-                    line = self.__home_dir + line[1:]
-                files.append(line)
+                files.append(self.__expanduser(line))
         return files
-    
+
+    def __expanduser(self, path):
+        if path.startswith("~"):
+            return self.__home_dir + path[1:]
+        return path
